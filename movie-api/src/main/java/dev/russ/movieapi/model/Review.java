9491025744e99d3221e16070e332694b206ee0bf
kiddo <johnrusselledomingo@gmail.com>
2024-03-21 00:31:36 +0800

    add review

--- movie-api/src/main/java/dev/russ/movieapi/model/Review.java
+++ movie-api/src/main/java/dev/russ/movieapi/model/Review.java
@@ -12,7 +12,11 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Review {
-    @Id
+    //@Id
     private ObjectId id;
     private String body;
+
+    public Review(String body) {
+        this.body = body;
+    }
 }
---
+++ movie-api/src/main/java/dev/russ/movieapi/repository/ReviewRepository.java
@@ -0,0 +1,11 @@
+package dev.russ.movieapi.repository;
+
+import dev.russ.movieapi.model.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;@Repository
+public interface ReviewRepository  extends MongoRepository<Review,ObjectId> {
+
+}
